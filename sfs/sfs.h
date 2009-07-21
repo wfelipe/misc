ec87395b519ec1637828aef9425615e2ec79f94b
Wilson Felipe <wfelipe@gmail.com>
2009-07-21 00:36:39 -0300

    implementations based on ramfs

---
+++ sfs/sfs.h
@@ -0,0 +1,5 @@
+static const struct super_operations sfs_s_ops;
+static const struct address_space sfs_aops;
+static const struct inode_operations sfs_file_inode_operations;
+static const struct inode_operations sfs_dir_inode_operations;
+static const struct file_operations sfs_file_operations;
