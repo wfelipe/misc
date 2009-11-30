4872da0c831108ef475a4fb7484bfbabebcd82a1
Wilson Felipe <wfelipe@gmail.com>
2009-11-30 09:26:24 -0200

    a simple example of implementing xmlrpc server in perl

--- programming/perl/rpc-xml-server.pl
+++ programming/perl/rpc-xml-server.pl
@@ -6,11 +6,22 @@ use strict;
 use warnings;
 
 use RPC::XML::Server;
+use Data::Dumper;
 
 my $srv = RPC::XML::Server->new (port => 9000);
 
 $srv->add_method ({	name => 'teste.wfelipe',
-			signature => [ 'int' ],
-			code => sub { return 1; } });
+	signature => [ 'string struct' ],
+	code => \&method
+});
 
-$srv->server_loop;
+$srv->server_loop (
+	server_type => qw( Fork Single ),
+	max_servers => 16,
+);
+
+sub method {
+	my %args = %{ shift () };
+
+	return "string";
+}
