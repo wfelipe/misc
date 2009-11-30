f3778332bbd608d00ad84e70ee0abfcb4413f92a
Wilson Felipe <wfelipe@gmail.com>
2009-11-30 21:38:14 -0200

    using an object to call longpoll function, once it seems to be more stable and controlled inside an unique object, instead of calling it everytime

--- programming/javascript/comet_longpoll/js/longpoll.js
+++ programming/javascript/comet_longpoll/js/longpoll.js
@@ -1,19 +1,19 @@
-function longpoll (lm)
-{
-	var xhReq = new XMLHttpRequest ();
+var Comet = {
+	longpoll: function (lm) {
+		var xhReq = new XMLHttpRequest ();
 
-	xhReq.open ("GET", "/broadcast/sub?id=3", true);
-	if (lm) {
-		xhReq.setRequestHeader ('If-Modified-Since', lm);
-	} else {
-		//xhReq.setRequestHeader ('If-Modified-Since', );
-		xhReq.setRequestHeader ('Cache-Control', 'no-cache');
-		xhReq.setRequestHeader ('Pragma', 'no-cache');
+		xhReq.open ("GET", "/broadcast/sub?id=3", true);
+		if (lm) {
+			xhReq.setRequestHeader ('If-Modified-Since', lm);
+		} else {
+			xhReq.setRequestHeader ('Cache-Control', 'no-cache');
+			xhReq.setRequestHeader ('Pragma', 'no-cache');
+		}
+		xhReq.onreadystatechange = function () {
+			if (xhReq.readyState != 4) { return; }
+			document.getElementById ('message').innerHTML = xhReq.responseText;
+			Comet.longpoll (xhReq.getResponseHeader ('Last-Modified'));
+		}
+		xhReq.send (null);
 	}
-	xhReq.onreadystatechange = function () {
-		if (xhReq.readyState != 4) { return; }
-		document.getElementById ('message').innerHTML = xhReq.responseText;
-		longpoll (xhReq.getResponseHeader ('Last-Modified'));
-	}
-	xhReq.send (null);
 }
