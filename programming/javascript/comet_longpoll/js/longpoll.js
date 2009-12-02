320f8b7e69391a391171e882134a7d76f55eeac2
Wilson Felipe <wfelipe@gmail.com>
2009-12-02 18:43:11 -0200

    now handles when the current object has the same last-modified header than the new one, which caused a loop until it updates

--- programming/javascript/comet_longpoll/js/longpoll.js
+++ programming/javascript/comet_longpoll/js/longpoll.js
@@ -6,13 +6,16 @@ var Comet = {
 		if (lm) {
 			xhReq.setRequestHeader ('If-Modified-Since', lm);
 		} else {
-			xhReq.setRequestHeader ('Cache-Control', 'no-cache');
-			xhReq.setRequestHeader ('Pragma', 'no-cache');
+			xhReq.setRequestHeader("If-Modified-Since", new Date (0).toGMTString ());
 		}
 		xhReq.onreadystatechange = function () {
 			if (xhReq.readyState != 4) { return; }
 			document.getElementById ('message').innerHTML = xhReq.responseText;
-			Comet.longpoll (xhReq.getResponseHeader ('Last-Modified'));
+			if (lm == xhReq.getResponseHeader ('Last-Modified')) {
+				Comet.longpoll (new Date (Date.parse (lm) + 1000).toGMTString ());
+			} else {
+				Comet.longpoll (xhReq.getResponseHeader ('Last-Modified'));
+			}
 		}
 		xhReq.send (null);
 	}
