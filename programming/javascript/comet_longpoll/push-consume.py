4ef78237f42974dfde149911f6e61bda89acc517
Wilson Felipe <wfelipe@wfelipe.(none)>
2010-03-07 13:50:34 -0300

    consume now reading from a file

--- programming/javascript/comet_longpoll/push-consume.py
+++ programming/javascript/comet_longpoll/push-consume.py
@@ -3,8 +3,11 @@
 import httplib, urllib
 import sys
 
-conn = httplib.HTTPConnection ("localhost:80")
-conn.request ("POST", "/broadcast/pub?id=3", sys.argv[1])
+fd = open (sys.argv[1])
+postdata = fd.read ()
+fd.close ()
+conn = httplib.HTTPConnection ("localhost:8080")
+conn.request ("POST", "/broadcast/pub?id=3", postdata)
 resp = conn.getresponse ()
 print resp.status, resp.reason
 data = resp.read ()
